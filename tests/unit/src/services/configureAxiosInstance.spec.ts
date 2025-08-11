eb71108e7668e7b6546ec5d6709b76dad00579a6
Spencer Howe <spencer.howe@justice.gov.uk>
2025-08-11 11:21:37 +0100

    feat: add secondary test case

--- tests/unit/src/services/configureAxiosInstance.spec.ts
+++ tests/unit/src/services/configureAxiosInstance.spec.ts
@@ -4,40 +4,61 @@ import { submissionsService } from '#src/services/submissionsService.js';
 import { AxiosInstance } from '#node_modules/axios/index.js';
 import config from '#config.js';
 
-describe("submissionsService:", () => {
+describe("configureAxios:", () => {
+    let baseUrlStub: string;
+    let axiosGetStub: sinon.SinonStub;
     let axiosInstanceStub: AxiosInstance;
     let axiosMiddlewareStub: {axiosInstance: AxiosInstance; use?: sinon.SinonStub};
-    let originalBaseUrl: string;    
-
+    let configStub: sinon.SinonStub;
+    
     beforeEach(() => {
-        
-    // Reset the stub before each test
-    axiosInstanceStub = {
-        defaults: {
-            baseURL: '',
-            headers: {
-                common: {} as Record<string, string>,
+        // Reset the stubs before each test
+        baseUrlStub = "fake.api"
+
+        configStub = sinon.stub(config, "api").value({
+            baseUrl: baseUrlStub
+        })
+
+        axiosGetStub = sinon.stub().resolves({data: []});
+
+        axiosInstanceStub = {
+            defaults: {
+                baseURL: '',
+                headers: {
+                    common: {} as Record<string, string>,
+                },
+                get: axiosGetStub
             },
-        },
-    } as unknown as AxiosInstance;
+        } as unknown as AxiosInstance;
 
-    axiosMiddlewareStub = {
-        axiosInstance: axiosInstanceStub,
-        use: sinon.stub(),
-    };
-
-    originalBaseUrl = config.api.baseUrl;
-    config.api.baseUrl = "FAKE"
+        axiosMiddlewareStub = {
+            axiosInstance: axiosInstanceStub,
+            use: sinon.stub(),
+        };
+    
     });
 
     afterEach(() => { 
-    // Restore the stubs after each test
-    config.api.baseUrl = originalBaseUrl;
-    sinon.restore();
+        sinon.restore();
     });
 
-    it("should configure axios instance", () => {
+    it("should configure axios instance in flow", async () => {
+        // Act - call the submissions service with stubbed config
+        await submissionsService.getSubmissions(axiosMiddlewareStub as any);
+
+        // Assert - the config stub has been inserted into the axios instance
+        expect(axiosInstanceStub.defaults.headers.common["Content-Type"]).to.equal("application/json");
+        expect(axiosInstanceStub.defaults.baseURL).to.equal(baseUrlStub);
+    })
+
+    // NOTE - the below test is FYI. The above method is 'better' but the below is a 'hacky' method which is good for reference hence inclusion.
+    it("should configure axios instance direct call", () => {
+        // Act - call the configure axios method directly
+        // JS override of the private modifier to allow
         const result = (submissionsService as any)['configureAxiosInstance'](axiosMiddlewareStub);
-        expect(result.axiosInstance.defaults.baseURL).to.equal("FAKE");
-  })
+
+        // Assert - the axios instance is configured as expected
+        expect(result.axiosInstance.defaults.baseURL).to.equal("fake.api");
+        expect(axiosInstanceStub.defaults.headers.common["Content-Type"]).to.equal("application/json");
+    })
 })
