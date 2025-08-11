2488243a9733b46560387a92b914e998e2b2c480
spennymoj <spencer.howe@justice.gov.uk>
2025-08-11 12:54:40 +0100

    feat: improve test coverage of submissionService (#66)
    * feat: improve test coverage
    
    * feat: add secondary test case
    
    * fix: formatting
    
    * fix: removing comments and amending structure

---
+++ tests/unit/src/services/configureAxiosInstance.spec.ts
@@ -0,0 +1,63 @@
+import sinon from "sinon";
+import { expect } from "chai";
+import { submissionsService } from "#src/services/submissionsService.js";
+import { AxiosInstance } from "#node_modules/axios/index.js";
+import config from "#config.js";
+
+describe("configureAxios:", () => {
+  let baseUrlStub: string;
+  let axiosGetStub: sinon.SinonStub;
+  let axiosInstanceStub: AxiosInstance;
+  let axiosMiddlewareStub: {
+    axiosInstance: AxiosInstance;
+    use?: sinon.SinonStub;
+  };
+  let configStub: sinon.SinonStub;
+
+  beforeEach(() => {
+    // Reset the stubs before each test
+    baseUrlStub = "fake.api";
+
+    configStub = sinon.stub(config, "api").value({
+      baseUrl: baseUrlStub,
+    });
+
+    axiosGetStub = sinon.stub().resolves({ data: [] });
+
+    axiosInstanceStub = {
+      defaults: {
+        baseURL: "",
+        headers: {
+          common: {} as Record<string, string>,
+        },
+        get: axiosGetStub,
+      },
+    } as unknown as AxiosInstance;
+
+    axiosMiddlewareStub = {
+      axiosInstance: axiosInstanceStub,
+      use: sinon.stub(),
+    };
+  });
+
+  afterEach(() => {
+    sinon.restore();
+  });
+
+  it("should configure axios instance in flow", async () => {
+    // Act - call the submissions service with stubbed config
+    await submissionsService.getSubmissions(axiosMiddlewareStub as any);
+
+    expect(axiosInstanceStub.defaults.headers.common["Content-Type"]).to.equal("application/json");
+    expect(axiosInstanceStub.defaults.baseURL).to.equal(baseUrlStub);
+  });
+
+  // NOTE - the below test is FYI. The above method is 'better' but the below is a 'hacky' method which is good for reference hence inclusion.
+  it("should configure axios instance direct call", () => {
+    // Calling the configure axios method directly - JS override of the private modifier
+    const result = (submissionsService as any)["configureAxiosInstance"](axiosMiddlewareStub);
+
+    expect(result.axiosInstance.defaults.baseURL).to.equal("fake.api");
+    expect(axiosInstanceStub.defaults.headers.common["Content-Type"]).to.equal("application/json");
+  });
+});
--- tests/unit/src/services/submissionsService.spec.ts
+++ tests/unit/src/services/submissionsService.spec.ts
@@ -1,128 +1,98 @@
-import sinon from 'sinon';
-import { expect } from 'chai';
-import getSubmissionTestData from '../testAssets/submissionTestData.js';
-import { submissionsService, transformSubmission } from '#src/services/submissionsService.js';
-import { isSubmission } from '#src/helpers/typeChecks.js';
-import { AxiosInstanceWrapper } from "#types/axios-instance-wrapper.js";
+import sinon from "sinon";
+import { expect } from "chai";
+import { submissionsService, transformSubmission } from "#src/services/submissionsService.js";
+import { isSubmission } from "#src/helpers/typeChecks.js";
+import { AxiosInstance } from "#node_modules/axios/index.js";
+import { getSubmissionsEndpoint } from "#src/api/apiEndpointConstants.js";
+import { data } from "#node_modules/cheerio/dist/esm/api/attributes.js";
 
 describe("submissionsService:", () => {
-
   describe("data transformer", () => {
-
     it("should transform a valid object into a submission type", () => {
-    
-    // Arrange - set up the mock data
-    const mockDataInput = {
-      id: "validId",
-      providerUserId: "validProviderUserId",
-      friendlyId: "validFriendlyId",
-      providerOfficeId: "validProviderOfficeId",
-      submissionTypeCode: "validSubmissionTypeCode",
-      submissionDate: "validSubmissionDate",
-      submissionPeriodStartDate: "validPeriodStart",
-      submissionPeriodEndDate: "validPeriodEnd",
-      scheduleId: "validScheduleId",
-      claims: undefined, // sitting as undefined -- not implemented yet
-    }
+      const mockDataInput = {
+        id: "validId",
+        providerUserId: "validProviderUserId",
+        friendlyId: "validFriendlyId",
+        providerOfficeId: "validProviderOfficeId",
+        submissionTypeCode: "validSubmissionTypeCode",
+        submissionDate: "validSubmissionDate",
+        submissionPeriodStartDate: "validPeriodStart",
+        submissionPeriodEndDate: "validPeriodEnd",
+        scheduleId: "validScheduleId",
+        claims: undefined, // sitting as undefined -- not implemented yet
+      };
 
-    // Act - call transformer with the mock data
-    const result = transformSubmission(mockDataInput);
+      const result = transformSubmission(mockDataInput);
 
+      // Check the data structures
 
-    // Assert - check the data 
-
-      // type guarding - defensive coding
-    expect(isSubmission(result)).to.be.true;
+      // type guarding
+      expect(isSubmission(result)).to.be.true;
 
       // checks this is a valid object (Submission is interface - doesn't exist at runtime)
-    expect(result).to.be.an('object'); 
+      expect(result).to.be.an("object");
 
       // checks the data conforms to the right structure
-    expect(result).to.have.property('id', 'validId'); 
-    expect(result).to.have.property('friendlyId', 'validFriendlyId'); 
-    expect(result).to.have.property('providerUserId', 'validProviderUserId'); 
-    expect(result).to.have.property('providerOfficeId', 'validProviderOfficeId'); 
-    expect(result).to.have.property('submissionTypeCode', 'validSubmissionTypeCode'); 
-    expect(result).to.have.property('submissionDate', 'validSubmissionDate'); 
-    expect(result).to.have.property('submissionPeriodStartDate', 'validPeriodStart'); 
-    expect(result).to.have.property('submissionPeriodEndDate', 'validPeriodEnd'); 
-    expect(result).to.have.property('scheduleId', 'validScheduleId'); 
+      expect(result).to.have.property("id", "validId");
+      expect(result).to.have.property("friendlyId", "validFriendlyId");
+      expect(result).to.have.property("providerUserId", "validProviderUserId");
+      expect(result).to.have.property("providerOfficeId", "validProviderOfficeId");
+      expect(result).to.have.property("submissionTypeCode", "validSubmissionTypeCode");
+      expect(result).to.have.property("submissionDate", "validSubmissionDate");
+      expect(result).to.have.property("submissionPeriodStartDate", "validPeriodStart");
+      expect(result).to.have.property("submissionPeriodEndDate", "validPeriodEnd");
+      expect(result).to.have.property("scheduleId", "validScheduleId");
 
-    expect(result.claims).to.be.an('array');
-    })
+      expect(result.claims).to.be.an("array");
+    });
 
     it("should throw an error when the transformer is used on null", () => {
-    
-    // Arrange - set up the mock data
-    const mockDataInput = null;
+      const mockDataInput = null;
 
-    // Act - call transformer with the mock data
-    // Assert - check it is thrown
-
-    expect(() => transformSubmission(mockDataInput)).to.throw('Invalid submissions item: expected object'); 
-    })
-  })
+      expect(() => transformSubmission(mockDataInput)).to.throw(
+        "Invalid submissions item: expected object"
+      );
+    });
+  });
 
   describe("getSubmissions", () => {
-    let submissionsServiceStub: sinon.SinonStub;
-    let logSpy: sinon.SinonSpy;
-    let errorSpy: sinon.SinonSpy;
-
-    let middleware: AxiosInstanceWrapper;
-
-    let req: Partial<Request>;
+    let axiosStub: sinon.SinonStubbedInstance<AxiosInstance>;
+    let configureAxiosStub: sinon.SinonStub;
 
     beforeEach(() => {
-    // Reset the stub before each test
-
-    submissionsServiceStub = sinon.stub(submissionsService, "getSubmissions");
-    logSpy = sinon.spy(console, 'log')
-    errorSpy = sinon.spy(console, 'error')
-
-    // Assign a mock or dummy value to middleware
-    middleware = {} as AxiosInstanceWrapper;
+      // Reset the stub before each test
+      axiosStub = { get: sinon.stub() } as any;
+      configureAxiosStub = sinon
+        .stub(submissionsService as any, "configureAxiosInstance")
+        .returns(axiosStub);
     });
 
-    afterEach(() => { 
-    // Restore the stubs after each test
-    submissionsServiceStub.restore();
-    logSpy.restore();
-    errorSpy.restore();
+    afterEach(() => {
+      // Restore the stubs after each test
+      sinon.restore();
     });
 
-    it("should make a GET request correctly", async () => {
-    // Mocking resolved value of the API call
-    submissionsServiceStub.resolves(getSubmissionTestData)
-    
-    // faked API call
-    const response = await submissionsService.getSubmissions(middleware);
+    it("should call axios with the submissions endpoint", async () => {
+      const mockEmptyData = { data: [] };
+      axiosStub.get.resolves(mockEmptyData);
 
-    // manually call the transform method
-    const responseTyped = transformSubmission(response);
+      await submissionsService.getSubmissions({} as any);
 
-    // Assertions
-    expect(response).to.deep.equal(getSubmissionTestData);
-    expect(isSubmission(responseTyped)).to.be.true;
-    expect(isSubmission(response)).to.be.false;
-    })
+      sinon.assert.calledWith(axiosStub.get, getSubmissionsEndpoint);
+      sinon.assert.calledWith(configureAxiosStub, {});
+    });
 
-    it("should throw an error when the API call fails", async () => {
-    submissionsServiceStub.rejects(new Error("API error"));
+    it("should return empty data and an error when axios fails", async () => {
+      const error = new Error("Network error");
+      axiosStub.get.rejects(error);
+      const errorMessage = "An unexpected error occurred. Please try again.";
 
-    try {
-        await submissionsService.getSubmissions(middleware);
-        throw new Error("should throw")
-    } catch (err ) {
-        if (err instanceof Error) {
-            expect(err.message).to.equal('API error');
-        } else {
-            throw err;
-        }
-    }
-    })
-  })
+      const result = await submissionsService.getSubmissions({} as any);
 
-})
-
-
-
+      expect(result).to.include({
+        message: errorMessage,
+      });
+      expect(result.data).to.be.an("array").that.is.empty;
+    });
+  });
+});
