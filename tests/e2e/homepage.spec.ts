59e8486b0690845b141bec20427e82203832a718
Spencer Howe <spencer.howe@justice.gov.uk>
2025-08-27 15:46:47 +0100

    fix: attempt new override

--- tests/e2e/homepage.spec.ts
+++ tests/e2e/homepage.spec.ts
@@ -2,13 +2,16 @@ import { getClaimsResponseData } from "#tests/assets/getClaimsResponseData.js";
 import { test, expect } from "@playwright/test";
 
 test("homepage should have the correct title", async ({ page }) => {
-  await page.route("**/api/v1/claims", async (route) => {
-    await route.fulfill({
-      status: 200,
-      contentType: "application/json",
-      body: JSON.stringify(getClaimsResponseData),
-    });
-  });
+  await page.route(
+    "http://laa-claim-for-payment.laa-claim-for-payment-uat:8080/api/v1/claims",
+    async (route) => {
+      await route.fulfill({
+        status: 200,
+        contentType: "application/json",
+        body: JSON.stringify(getClaimsResponseData),
+      });
+    }
+  );
 
   // Navigate to the homepage
   await page.goto("/");
