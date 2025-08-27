b0296407e820e4895e9d4d0888054e1c00dc0b57
Spencer Howe <spencer.howe@justice.gov.uk>
2025-08-27 16:22:15 +0100

    fix: attempt to override call to inaccessible API version

--- tests/e2e/homepage.spec.ts
+++ tests/e2e/homepage.spec.ts
@@ -2,6 +2,14 @@ import { getClaimsResponseData } from "#tests/assets/getClaimsResponseData.js";
 import { test, expect } from "@playwright/test";
 
 test("homepage should have the correct title", async ({ page }) => {
+  await page.route("http://laa-claim-for-payment.laa-claim-for-payment-uat:8080", async (route) => {
+    await route.fulfill({
+      status: 200,
+      contentType: "application/json",
+      body: JSON.stringify(getClaimsResponseData),
+    });
+  });
+
   // Navigate to the homepage
   await page.goto("/");
 
