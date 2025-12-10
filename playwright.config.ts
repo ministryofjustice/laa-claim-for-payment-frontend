cf087d02d2b777198677fefaf6d21157415dd1d5
Adam Coles <adam.coles@justice.gov.uk>
2025-12-10 08:54:45 +0000

    disable redis 1

--- playwright.config.ts
+++ playwright.config.ts
@@ -42,6 +42,7 @@ export default defineConfig({
       ...PLAYWRIGHT_TEST_ENV,
       //TODO: for now disable auth for e2e tests
       AUTH_ENABLED: 'false',
+      DISABLE_REDIS: 'true',
     },
   },
 });
