cf81e51544c2f7188f22d47f4bec4dd5e0380d79
liampan <liam.oldfield@justice.gov.uk>
2026-03-23 17:18:04 +0000

    CLAS-212 | Load latest CSS

--- utils/buildHelper.ts
+++ utils/buildHelper.ts
@@ -1,7 +1,6 @@
 import fs from 'node:fs';
 
 const RANDOM_NUMBER_UPPER_BOUND = 10000;
-const FIRST_IN_ARRAY = 0;
 
 /**
  * Generate a random build number as a string.
@@ -16,9 +15,24 @@ export const getBuildNumber = (): string => Math.floor(Math.random() * RANDOM_NU
  * @param {string} extension - The extension of the build files.
  * @returns {string} - The name of the latest build file or an empty string if none found.
  */
-export const getLatestBuildFile = (directory: string, prefix: string, extension: string): string => {
+export const getLatestBuildFile = (
+  directory: string,
+  prefix: string,
+  extension: string
+): string => {
   const files = fs.readdirSync(directory);
-  const pattern = new RegExp(`^${prefix}\\.\\d+\\.${extension}$`);
-  const matchingFiles = files.filter(file => pattern.test(file));
-  return matchingFiles.length > FIRST_IN_ARRAY ? matchingFiles[FIRST_IN_ARRAY] : '';
+
+  const matchingFiles = files
+    .filter(file => new RegExp(`^${prefix}\\.\\d+\\.${extension}$`).test(file))
+    .map(file => ({
+      file,
+      mtime: fs.statSync(`${directory}/${file}`).mtime.getTime(),
+    }))
+    .sort((a, b) => b.mtime - a.mtime);
+
+  if (matchingFiles.length === 0) {
+    return "";
+  }
+
+  return matchingFiles[0].file;
 };
