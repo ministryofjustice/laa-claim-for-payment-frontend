355e99e14e824769614d704efb68fc8fd120b93c
jackheslop96 <43777106+jackheslop96@users.noreply.github.com>
2026-05-13 08:58:31 +0100

    CLAS-371: OIDC changes to pass against ZAP's authentication tester.

--- utils/openidSetup.ts
+++ utils/openidSetup.ts
@@ -119,10 +119,10 @@ export const oidcSetup = (app: Application): void => {
   app.get(LOGIN_PATH, async (req: Request, res: Response) => {
     const config = await getConfig();
 
-    const redirectUri = `${BASE_URL}${CALLBACK_PATH}`;
-    const codeVerifier = randomPKCECodeVerifier();
+    const redirectUri = req.session.oidc?.redirect_uri ?? `${BASE_URL}${CALLBACK_PATH}`;
+    const codeVerifier = req.session.oidc?.code_verifier ?? randomPKCECodeVerifier();
     const codeChallenge = await calculatePKCECodeChallenge(codeVerifier);
-    const state = randomState();
+    const state = req.session.oidc?.state ?? randomState();
 
     // Persist flow state in the session
     req.session.oidc = {
@@ -168,20 +168,24 @@ export const oidcSetup = (app: Application): void => {
 
       const idTokenPayload = decodeIdToken(tokens.id_token);
 
-      req.session.oidc = {
-        ...sess,
-        tokens,
-        userinfo: idTokenPayload, 
-      };
+      req.session.regenerate((err) => {
+        if (err != null) {
+          next(err);
+          return;
+        }
 
-      // redirect to home page
-      res.redirect('/');
+        req.session.oidc = {
+          tokens,
+          userinfo: idTokenPayload,
+        };
+
+        // redirect to home page
+        res.redirect('/');
+      });
     } catch (err) {
-
       handleCallbackError(err, req);
-
-  next(err);
-}
+      next(err);
+    }
   });
 
   // GET /logout -> clear session, optionally call OP logout if available
