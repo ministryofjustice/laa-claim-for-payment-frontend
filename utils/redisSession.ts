d87bb372e677580c9c6b2d43495b98b3bd319ee7
liampan <liam.oldfield@justice.gov.uk>
2026-06-30 12:30:03 +0100

    enable insecure session for regression

--- utils/redisSession.ts
+++ utils/redisSession.ts
@@ -27,7 +27,7 @@ export const setupRedisSession = (app: Application, redisClient: RedisClientType
             resave: false,
             saveUninitialized: false,
             cookie: {
-                secure: process.env.NODE_ENV !== 'development',
+                secure: process.env.SESSION_COOKIE_SECURE !== 'false',
                 httpOnly: true,
                 maxAge: 1000 * 60 * 60 * 3,
             },
