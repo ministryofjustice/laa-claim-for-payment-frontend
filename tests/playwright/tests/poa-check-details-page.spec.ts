b2b07b28c01bf5c5ecf159d95d796ddb159e5d58
jackheslop96 <43777106+jackheslop96@users.noreply.github.com>
2026-07-29 22:30:19 +0100

    CLAS-520: Fixed playwright test.

--- tests/playwright/tests/poa-check-details-page.spec.ts
+++ tests/playwright/tests/poa-check-details-page.spec.ts
@@ -1,8 +1,11 @@
 import { expect, test } from "../fixtures/index.js";
-import { claim1Id } from "#tests/playwright/factories/handlers/api.js";
+import {
+  claim1Id,
+  profitCostDraftClaim1Id,
+} from "#tests/playwright/factories/handlers/api.js";
 
 test("page should have the correct title", async ({ page }) => {
-  await page.goto(`/claims/${claim1Id}/poa/check-details`);
+  await page.goto(`/claims/${profitCostDraftClaim1Id}/poa/check-details`);
 
   await expect(page).toHaveTitle(
     "Check your details – Claim for controlled work – GOV.UK",
@@ -13,7 +16,7 @@ test("page displays the correct content", async ({
   pages,
   checkAccessibility,
 }) => {
-  const page = pages.poaCheckDetailsPage(claim1Id);
+  const page = pages.poaCheckDetailsPage(profitCostDraftClaim1Id);
 
   await page.navigate();
   await page.waitForLoad();
