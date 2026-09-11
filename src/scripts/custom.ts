fed071369bc1026d8011e6a95600fc6aed1083cb
jackheslop96 <43777106+jackheslop96@users.noreply.github.com>
2026-09-11 11:19:38 +0100

    CLM-197: Using cache control headers so that forms are filled appropriately upon clicking back.

--- src/scripts/custom.ts
+++ src/scripts/custom.ts
@@ -3,8 +3,17 @@ import "#src/scripts/asciiArt.js";
 import '#src/scripts/multiFileUpload.js';
 
 document.querySelectorAll('[data-module="back-link"]').forEach((link) => {
-  link.addEventListener("click", (e) => {
-    e.preventDefault();
-    window.history.back();
-  });
+  if (window.history) {
+    // prevent resubmit warning
+    if (window.history.replaceState && typeof window.history.replaceState === 'function') {
+      window.history.replaceState(null, "", window.location.href);
+    }
+
+    link.addEventListener('click', (event) => {
+      event.preventDefault();
+      if (window.history.back && typeof window.history.back === 'function') {
+        window.history.back();
+      }
+    });
+  }
 });
