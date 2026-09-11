8614a231f021b29bd58a8ab1ff9dbba4bda85924
jackheslop96 <43777106+jackheslop96@users.noreply.github.com>
2026-09-11 11:26:40 +0100

    CLM-197: Linting

--- src/scripts/custom.ts
+++ src/scripts/custom.ts
@@ -3,17 +3,11 @@ import "#src/scripts/asciiArt.js";
 import '#src/scripts/multiFileUpload.js';
 
 document.querySelectorAll('[data-module="back-link"]').forEach((link) => {
-  if (window.history) {
-    // prevent resubmit warning
-    if (window.history.replaceState && typeof window.history.replaceState === 'function') {
-      window.history.replaceState(null, "", window.location.href);
-    }
+  // prevent ERR_CACHE_MISS (Confirm Form Resubmission) error
+  window.history.replaceState(null, "", window.location.href);
 
-    link.addEventListener('click', (event) => {
-      event.preventDefault();
-      if (window.history.back && typeof window.history.back === 'function') {
-        window.history.back();
-      }
-    });
-  }
+  link.addEventListener("click", (event) => {
+    event.preventDefault();
+    window.history.back();
+  });
 });
